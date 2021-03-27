c5d02e58fae168de288c30dc50bba010ea6fd6fc
Callum Skeet <callum@elitemail.org>
2021-03-27 12:42:20 +1100

    create training scripts

---
+++ train_xception.py
@@ -0,0 +1,143 @@
+import sys
+from utils import *
+
+DATA_PARAMS = dict(
+    target_size=(64, 64),
+    batch_size=16,
+    directory="dataset/simpsons_train/",
+    seed=1337,
+    image_data_generator=ImageDataGenerator(validation_split=0.2),
+    class_mode="categorical",
+    interpolation="bicubic",
+)
+
+TRAIN_PARAMS = dict(learning_rate=1e-4, dropout=0.3, epochs=1)
+
+
+def make_xception(input_shape=(128, 128), dropout=0.3):
+    inputs = keras.Input(shape=input_shape + (3,))
+
+    # Entry block
+    x = layers.experimental.preprocessing.Rescaling(1.0 / 255)(inputs)
+    x = layers.experimental.preprocessing.RandomWidth(0.3)(x)
+    x = layers.experimental.preprocessing.RandomHeight(0.3)(x)
+    x = layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical")(x)
+    x = layers.experimental.preprocessing.RandomRotation(0.3)(x)
+
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.Conv2D(64, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # set aside residual
+
+    for size in [128, 256, 512, 728]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.GlobalAveragePooling2D()(x)
+
+    x = layers.Dropout(dropout)(x)
+    outputs = layers.Dense(units=20, activation="softmax")(x)
+
+    return keras.Model(inputs, outputs)
+
+
+def train(
+    train_ds,
+    val_ds,
+    learning_rate=1e-4,
+    epochs=1,
+    input_shape=(299, 299),
+    dropout=0.3,
+    optimizer=tf.keras.optimizers.Adam,
+    callbacks=None,
+):
+    model = make_xception(input_shape=input_shape, dropout=dropout)
+
+    model.compile(
+        optimizer=optimizer(learning_rate=learning_rate),
+        loss="categorical_crossentropy",
+        metrics="accuracy",
+    )
+
+    history = model.fit(
+        train_ds, epochs=epochs, callbacks=callbacks, validation_data=val_ds
+    )
+
+    return model, history
+
+
+def main():
+    init_gpu()
+    wandb.init(project="deakin-simpsons", entity="cask", mode="offline")
+    if len(sys.argv) > 1:
+        args = vars(parse_args())
+        args = {k: v for k, v in args.items() if v is not None}
+        TRAIN_PARAMS.update(args)
+
+    train_ds, val_ds, class_names, num_classes = create_datasets(DATA_PARAMS)
+    config = {
+        "input_shape": DATA_PARAMS["target_size"],
+        "learning_rate": TRAIN_PARAMS["learning_rate"],
+        "dropout": TRAIN_PARAMS["dropout"],
+        "epochs": TRAIN_PARAMS["epochs"],
+    }
+    wandb.config.update(config)
+
+    callbacks = [
+        keras.callbacks.EarlyStopping(monitor="val_accuracy", patience=5),
+        WandbCallback(
+            monitor="val_accuracy",
+            mode="max",
+            training_data=train_ds,
+            validation_data=val_ds,
+            data_type="image",
+            labels=class_names,
+            log_gradients=True,
+        ),
+    ]
+
+    model, history = train(train_ds, val_ds, callbacks=callbacks, **config)
+
+    val_ds.reset()
+    val_ds.shuffle = False
+    val_ds.next()
+    y_prob = model.predict(val_ds)
+    y_pred = y_prob.argmax(axis=-1)
+    y_true = val_ds.labels
+
+    wandb.sklearn.plot_confusion_matrix(y_true, y_pred, class_names)
+
+    wandb.log(
+        {
+            "classification_report": classification_report(
+                y_true, y_pred, target_names=class_names, output_dict=True
+            )
+        }
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,166 @@
+import os
+import itertools
+import collections
+import datetime
+import argparse
+from pathlib import Path
+
+import numpy as np
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import h5py
+import wandb
+from wandb.keras import WandbCallback
+
+from sklearn.metrics import (
+    confusion_matrix,
+    balanced_accuracy_score,
+    accuracy_score,
+    classification_report,
+)
+
+import tensorflow as tf
+from tensorflow import keras
+
+from tensorflow.keras import models, layers, optimizers
+from tensorflow.python.keras.saving import hdf5_format
+from keras.preprocessing.image import ImageDataGenerator, DirectoryIterator
+from tensorboard.plugins.hparams import api as hp
+
+
+DataParams = collections.namedtuple(
+    "DataParams",
+    [
+        "target_size",
+        "batch_size",
+        "directory",
+        "seed",
+        "image_data_generator",
+        "class_mode",
+        "interpolation",
+        "class_names",
+    ],
+)
+
+HyperParams = collections.namedtuple(
+    "HyperParams", ["learning_rate", "dropout", "epochs"]
+)
+
+
+class ModelCheckpoint_tweaked(tf.keras.callbacks.ModelCheckpoint):
+    def __init__(
+        self,
+        filepath,
+        monitor="val_loss",
+        verbose=0,
+        save_best_only=False,
+        save_weights_only=False,
+        mode="auto",
+        save_freq="epoch",
+        **kwargs,
+    ):
+
+        # Change tf_utils source package.
+        from tensorflow.python.keras.utils import tf_utils
+
+        super().__init__(
+            filepath,
+            monitor,
+            verbose,
+            save_best_only,
+            save_weights_only,
+            mode,
+            save_freq,
+            **kwargs,
+        )
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--learning-rate")
+    parser.add_argument("--dropout")
+    parser.add_argument("--epochs")
+    return parser.parse_args()
+
+
+def init_gpu():
+    gpus = tf.config.list_physical_devices("GPU")
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices("GPU")
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
+
+
+def create_datasets(data_params):
+    # Now, we create a training data iterator by creating batchs of images of the same size as
+    # defined previously, i.e., each image is resized in a 64x64 pixels format.
+    train_ds = DirectoryIterator(
+        **data_params,
+        subset="training",
+    )
+
+    # Similarly, we create a validation data iterator by creating batchs of images of the same size as
+    # defined previously, i.e., each image is resized in a 64x64 pixels format.
+    val_ds = DirectoryIterator(**data_params, subset="validation", shuffle=False)
+
+    # We save the list of classes (labels).
+    class_names = list(train_ds.class_indices.keys())
+
+    # We also save the number of labels.
+    num_classes = train_ds.num_classes
+
+    return train_ds, val_ds, class_names, num_classes
+
+
+def plot_class_distribution(train_ds, class_names):
+    """Plots the distribution of examples for each class"""
+    counter = collections.Counter(train_ds.labels)
+    v = [[class_names[item[0]], item[1]] for item in counter.items()]
+    df = pd.DataFrame(data=v, columns=["index", "value"])
+
+    g = sns.catplot(
+        x="index",
+        y="value",
+        data=df,
+        kind="bar",
+        legend=False,
+        height=4,
+        aspect=4,
+        saturation=1,
+    )
+    g.despine(top=False, right=False)
+
+    plt.xlabel("Classes")
+    plt.ylabel("#images")
+    plt.title("Distribution of images per class")
+    plt.xticks(rotation="vertical")
+    plt.show()
+    return g
+
+
+def plot_data(train_ds, class_names):
+    """Displays 30 images from the training set"""
+    plt.figure(figsize=(20, 16))
+    images = []
+    labels = []
+    for itr in train_ds.next():
+        for i in range(30):
+            if len(images) < 30:
+                images.append(itr[i].astype("uint8"))
+            else:
+                labels.append(list(itr[i]).index(1))
+
+    for i in range(len(images)):
+        ax = plt.subplot(5, 6, i + 1)
+        plt.imshow(images[i])
+        plt.title(
+            class_names[labels[i]].replace("_", " ") + " (" + str(int(labels[i])) + ")"
+        )
+        plt.axis("off")
